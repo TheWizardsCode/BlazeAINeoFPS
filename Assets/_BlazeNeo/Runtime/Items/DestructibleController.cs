363ff06f7839b02528b0addca15191d56e3de1a7
Ross Gardler <ross.gardler@microsoft.com>
2022-05-29 00:59:06 -0700

    More destructibles

--- Assets/_BlazeNeo/Runtime/Items/DestructibleController.cs
+++ Assets/_BlazeNeo/Runtime/Items/DestructibleController.cs
@@ -8,15 +8,17 @@ namespace WizardsCode.BlazeNeoFPS
     /// <summary>
     /// Manages the health of a destructible object and will destroy the object when it "dies"
     /// </summary>
-    [RequireComponent(typeof(BasicHealthManager))]
     [RequireComponent(typeof(BasicDamageHandler))]
     [RequireComponent(typeof(SimpleSurface))]
+    [RequireComponent(typeof(Collider))]
     public class DestructibleController : BasicHealthManager
     {
         [SerializeField, Tooltip("The particle effect to replace the object with when it is destroyed.")]
         GameObject m_DestructionParticles;
-        [SerializeField, Tooltip("Density of particles to spawn. The higher this value the more particals will spawn."), Range(0.5f, 100)]
+        [SerializeField, Tooltip("Density of particles to spawn. The higher this value the more particles will spawn."), Range(0.5f, 100)]
         float m_ParticalDensity = 25;
+        [SerializeField, Tooltip("Explosive force. The higher this value the more the particles will move away from the center of the destructable object."), Range(0f, 10f)]
+        float m_ExplosiveForce = 3;
         
 
         protected override void OnIsAliveChanged()
@@ -46,6 +48,7 @@ namespace WizardsCode.BlazeNeoFPS
                     ParticleSystem.ShapeModule shape;
                     ParticleSystem.EmissionModule emission;
                     ParticleSystem.Burst burst;
+                    ParticleSystem.ForceOverLifetimeModule force;
                     for (int i = 0; i < ps.Length; i++)
                     {
                         shape = ps[i].shape;
@@ -55,6 +58,18 @@ namespace WizardsCode.BlazeNeoFPS
                         burst = emission.GetBurst(0);
                         burst.count = m_ParticalDensity * boundsMultiplier;
                         emission.SetBurst(0, burst);
+
+                        force = ps[i].forceOverLifetime;
+                        if (m_ExplosiveForce > 0)
+                        {
+                            force.enabled = true;
+                            force.x = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                            force.y = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                            force.z = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                        } else
+                        {
+                            force.enabled = false;
+                        }
 
                         ps[i].GetComponent<ParticleSystemRenderer>().material = meshRenderer.material;
                     }
