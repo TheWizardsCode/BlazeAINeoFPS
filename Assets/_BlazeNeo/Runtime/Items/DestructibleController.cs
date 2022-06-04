8dcf6fdf5c476370f1fdbc8fd50f8ca88698751e
Ross Gardler <rgardler@apache.org>
2022-06-04 08:35:37 -0700

    Some tuning of destructibles, start on level 2

--- Assets/_BlazeNeo/Runtime/Items/DestructibleController.cs
+++ Assets/_BlazeNeo/Runtime/Items/DestructibleController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using NeoFPS;
+using UnityEngine.Serialization;
 
 namespace WizardsCode.BlazeNeoFPS
 {
@@ -13,12 +14,15 @@ namespace WizardsCode.BlazeNeoFPS
     [RequireComponent(typeof(Collider))]
     public class DestructibleController : BasicHealthManager
     {
-        [SerializeField, Tooltip("The particle effect to replace the object with when it is destroyed.")]
-        GameObject m_DestructionParticles;
+        [SerializeField, Tooltip("The particle effect to replace the object with when it is destroyed. These effects will be adjusted to match the obnject being destroyed.")]
+        [FormerlySerializedAs("m_DestructionParticles")]
+        GameObject[] m_ScaledDestructionParticles;
         [SerializeField, Tooltip("Density of particles to spawn. The higher this value the more particles will spawn."), Range(0.5f, 100)]
         float m_ParticalDensity = 25;
         [SerializeField, Tooltip("Explosive force. The higher this value the more the particles will move away from the center of the destructable object."), Range(0f, 10f)]
         float m_ExplosiveForce = 3;
+        [SerializeField, Tooltip("The particle effect to replace the object with when it is destroyed. These effects will be adjusted to match the obnject being destroyed.")]
+        GameObject[] m_UnscaledDestructionParticles;
 
         [Header("Fall Damage")]
         [SerializeField, Tooltip("A multiplier for fall damage. 0 means no damage will be taken.")]
@@ -29,16 +33,13 @@ namespace WizardsCode.BlazeNeoFPS
         {
             if (!isAlive)
             {
-                if (m_DestructionParticles != null)
+
+                if (m_ScaledDestructionParticles != null)
                 {
-                    //OPTIMIZATION: Use a pool system for the destruction particles
-                    GameObject go = Instantiate(m_DestructionParticles, transform);
-                    //OPTIMIZATION: cache on start
-                    ParticleSystem[] ps = go.GetComponentsInChildren<ParticleSystem>();
                     //OPTIMIZATION: cache on start
                     MeshRenderer meshRenderer = GetComponentInChildren<MeshRenderer>();
                     meshRenderer.enabled = false;
-                    
+
                     //OPTIMIZATION: cache on start
                     Collider[] colliders = GetComponentsInChildren<Collider>();
                     for (int i = 0; i < colliders.Length; i++)
@@ -47,39 +48,66 @@ namespace WizardsCode.BlazeNeoFPS
                     }
 
                     int boundsMultiplier = 2 * (int)(meshRenderer.bounds.extents.x + meshRenderer.bounds.extents.y + meshRenderer.bounds.extents.z);
-                    Debug.Log($"Bounds multiplier {boundsMultiplier}");
+                    //Debug.Log($"Bounds multiplier {boundsMultiplier}");
 
-                    ParticleSystem.ShapeModule shape;
-                    ParticleSystem.EmissionModule emission;
-                    ParticleSystem.Burst burst;
-                    ParticleSystem.ForceOverLifetimeModule force;
-                    for (int i = 0; i < ps.Length; i++)
+                    //OPTIMIZATION: Use a pool system for the destruction particles
+                    for (int d = 0; d < m_ScaledDestructionParticles.Length; d++)
                     {
-                        shape = ps[i].shape;
-                        shape.meshRenderer = meshRenderer;
+                        GameObject go = Instantiate(m_ScaledDestructionParticles[d]);
+                        go.transform.SetParent(transform);
+                        go.transform.localPosition = Vector3.zero;
 
-                        emission = ps[i].emission;
-                        burst = emission.GetBurst(0);
-                        burst.count = m_ParticalDensity * boundsMultiplier;
-                        emission.SetBurst(0, burst);
+                        ParticleSystem[] ps = go.GetComponentsInChildren<ParticleSystem>();
 
-                        force = ps[i].forceOverLifetime;
-                        if (m_ExplosiveForce > 0)
+                        ParticleSystem.ShapeModule shape;
+                        ParticleSystem.EmissionModule emission;
+                        ParticleSystem.Burst burst;
+                        ParticleSystem.ForceOverLifetimeModule force;
+                        for (int i = 0; i < ps.Length; i++)
                         {
-                            force.enabled = true;
-                            force.x = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
-                            force.y = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
-                            force.z = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
-                        } else
-                        {
-                            force.enabled = false;
+                            shape = ps[i].shape;
+                            if (shape.shapeType == ParticleSystemShapeType.MeshRenderer)
+                            {
+                                shape.meshRenderer = meshRenderer;
+                                ps[i].GetComponent<ParticleSystemRenderer>().material = meshRenderer.material;
+                            }
+
+                            emission = ps[i].emission;
+                            burst = emission.GetBurst(0);
+                            burst.count = m_ParticalDensity * boundsMultiplier;
+                            emission.SetBurst(0, burst);
+
+                            force = ps[i].forceOverLifetime;
+                            if (m_ExplosiveForce > 0)
+                            {
+                                force.enabled = true;
+                                force.x = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                                force.y = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                                force.z = Random.Range(-m_ExplosiveForce, m_ExplosiveForce);
+                            }
+                            else
+                            {
+                                force.enabled = false;
+                            }
                         }
 
-                        ps[i].GetComponent<ParticleSystemRenderer>().material = meshRenderer.material;
+                        Destroy(go, 10);
                     }
-
-                    Destroy(gameObject, 10);
                 }
+
+                if (m_UnscaledDestructionParticles != null)
+                {
+                    //OPTIMIZATION: Use a pool system for the destruction particles
+                    for (int d = 0; d < m_UnscaledDestructionParticles.Length; d++)
+                    {
+                        GameObject go = Instantiate(m_UnscaledDestructionParticles[d]);
+                        go.transform.SetParent(transform);
+                        go.transform.localPosition = Vector3.zero;
+
+                    }
+                }
+
+                Destroy(gameObject, 15);
             }
             base.OnIsAliveChanged();
         }
